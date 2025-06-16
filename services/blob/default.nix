a733e7938169d05dfe0616d285b672f0fb436edd
Ethan Thoma <ethanthoma@gmail.com>
2025-06-16 13:18:11 -0700

    tidy: remove blob storage service (unused)

--- services/blob/default.nix
+++
@@ -1,9 +0,0 @@
-{ buildGoApplication }:
-buildGoApplication rec {
-  pname = "blob";
-  version = "0.1.0";
-  src = ../../.;
-  pwd = ./.;
-  modules = ../../gomod2nix.toml;
-  subPackages = [ "services/${pname}" ];
-}
